242a41b349b4aaa12dde1eb3f4c4430bf3e2b1d8
ongboonhan <edvinongbh@gmail.com>
2022-11-26 17:25:22 +0800

    update screen

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StatusBar, StyleSheet, View } from "react-native";
+import { LOGIN_SCREEN } from "./constants";
+import LoginScreen from "./screens/LoginScreen";
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
+  const [loading, setLoading] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  async function setToken() {
+    const token = await AsyncStorage.getItem("token");
+    // if there is not token, it will be falsey value
+    setLoggedIn(token);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setToken();
+  }, []);
+
+  const LoadingScreen = () => (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your !</Text>
-      <StatusBar style="auto" />
+      <ActivityIndicator />
     </View>
   );
+  const AppScreen = () => (
+    <NavigationContainer>
+      <StatusBar />
+      <Stack.Navigator
+        initialRouteName={LOGIN_SCREEN}
+        screenOptions={{
+          animationEnabled: false,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen component={LoginScreen} name={LOGIN_SCREEN} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  return loading ? <LoadingScreen /> : <AppScreen />;
 }
 
 const styles = StyleSheet.create({
